0436fab9f8e853854372dab4a5f97192488d8dd0
ASrivatsav27 <srivatsava47@gmail.com>
2025-11-06 22:36:24 +0530

    Re-added http_server folder with index.js file

---
+++ http_server/index.js
@@ -0,0 +1,10 @@
+const http = require('node:http')
+const server = http.createServer(function (req, res) {
+    console.log('I got an incoming request')
+    res.writeHead(200); // http status code
+    res.end('Thanks for visiting my server :)');// message to the client
+});
+
+server.listen(8000, function () {
+    console.log(`Http server is up and running on port 8000`)
+})
